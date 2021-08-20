a78fc09925c0df7600060f69e87a3b8646ed6e00
Sammy Robens-Paradise <srobensparadise@gmail.com>
2021-08-20 14:30:23 -0700

    added old school style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import OldSchool from '../providers/oldschool'
-function Portfolio({ Component, pageProps }: AppProps) {
+import React from 'react'
+function Portfolio({ Component, pageProps, router }: AppProps) {
   return (
     <OldSchool.Provider>
-      <Component {...pageProps} />
+      <Wrapper Component={Component} pageProps={pageProps} router={router} />
     </OldSchool.Provider>
   )
 }
+
+function Wrapper({ Component, pageProps }: AppProps) {
+  const OS = OldSchool.useOldSchool()
+  if (OS?.state) {
+    return (
+      <div>
+        <button onClick={() => OS.hideOldSchool()}>click me</button>
+      </div>
+    )
+  }
+  return <Component {...pageProps} />
+}
+
 export default Portfolio
