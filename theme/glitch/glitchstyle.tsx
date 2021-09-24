ed01f6db3e74c7943815b11b7acc9971d0679d20
Sammy Robens-Paradise <srobensparadise@gmail.com>
2021-09-24 15:35:47 -0400

    changes

--- theme/glitch/glitchstyle.tsx
+++ theme/glitch/glitchstyle.tsx
@@ -1,16 +1,36 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 
-const GlitchRenderer = (): ReactElement => (
-  <div>
-    <div className="flex items-center justify-between align-center p-40">
-      <div className="gl-center">
-        <div className="glitch-effect" data-text="Loading...">
-          Loading...
+const message = 'Welcome.'
+const GlitchRenderer = (): ReactElement => {
+  const [printMessage, updatePrintMessage] = useState<string>('')
+  const [printMessageLetterCount, updatePrintMessageLetterCount] =
+    useState<number>(0)
+
+  useEffect(() => {
+    const typeInterval = setInterval(() => {
+      if (printMessageLetterCount < message.length) {
+        updatePrintMessage(`${printMessage}${message[printMessageLetterCount]}`)
+        updatePrintMessageLetterCount(printMessageLetterCount + 1)
+      } else {
+        clearInterval(typeInterval)
+      }
+    }, 100)
+    return () => {
+      clearInterval(typeInterval)
+    }
+  })
+  return (
+    <div>
+      <div className="flex items-center justify-between align-center p-40">
+        <div className="gl-center">
+          <div className="glitch-effect" data-text={printMessage}>
+            {printMessage}
+          </div>
+          <div className="glow">{printMessage}</div>
         </div>
-        <div className="glow">Loading...</div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default GlitchRenderer
