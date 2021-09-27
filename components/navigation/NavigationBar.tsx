2eb73e9d22744f840e6baf1cbf83b57656686ed6
Sammy Robens-Paradise <srobensparadise@gmail.com>
2021-09-26 19:19:11 -0700

    F

--- components/navigation/NavigationBar.tsx
+++ components/navigation/NavigationBar.tsx
@@ -1,9 +1,14 @@
 import React, { ReactElement } from 'react'
 
+import Puzzle from '../../global/assets/puzzle.svg'
+
 const NavigationBar = (): ReactElement => (
   <div className="text-cerulaen dark:text-off-white grid grid-cols-3 gap-16 p-6 text-lg font-work font-normal">
     <div className="flex flex-row">1</div>
-    <div className="text-center flex-row">Sammy Robens-Paradise</div>
+    <div className="text-center flex flex-row space-x-4 justify-center">
+      <Puzzle />
+      <div>Sammy Robens-Paradise</div>
+    </div>
     <div className="flex flex-row justify-end">3</div>
   </div>
 )
