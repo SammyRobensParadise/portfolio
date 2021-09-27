7ee4ebd85ba33c5a662a590291529765107e85eb
Sammy Robens-Paradise <srobensparadise@gmail.com>
2021-09-27 09:34:01 -0700

    added icons

--- components/navigation/NavigationBar.tsx
+++ components/navigation/NavigationBar.tsx
@@ -1,15 +1,38 @@
 import React, { ReactElement } from 'react'
+import Link from 'next/link'
 
 import Puzzle from '../../global/assets/puzzle.svg'
+import Icon from '../../global/assets/Icon.svg'
 
 const NavigationBar = (): ReactElement => (
   <div className="text-cerulaen dark:text-off-white grid grid-cols-3 gap-16 p-6 text-lg font-work font-normal">
-    <div className="flex flex-row">1</div>
-    <div className="text-center flex flex-row space-x-4 justify-center">
-      <Puzzle />
-      <div>Sammy Robens-Paradise</div>
+    <div className="flex flex-row">
+      <Link href="/" passHref>
+        <Icon />
+      </Link>
     </div>
-    <div className="flex flex-row justify-end">3</div>
+    <div className="text-center flex flex-row space-x-4 justify-center hover:underline cursor-pointer pt-2">
+      <Link href="/" passHref>
+        <>
+          <Puzzle />
+          <p>Sammy Robens-Paradise</p>
+        </>
+      </Link>
+    </div>
+    <div className="flex flex-row justify-end space-x-4 pt-2">
+      <Link href="/resume" passHref>
+        <p className="hover:underline transition-all cursor-pointer">Résume</p>
+      </Link>
+      <Link href="/projects" passHref>
+        <p className="hover:underline cursor-pointer">Projects</p>
+      </Link>
+      <Link href="/work" passHref>
+        <p className="hover:underline cursor-pointer">Work</p>
+      </Link>
+      <Link href="/about" passHref>
+        <p className="hover:underline cursor-pointer">About</p>
+      </Link>
+    </div>
   </div>
 )
 
