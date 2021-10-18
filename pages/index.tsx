ec7cd9f1a0c19ec24e8561c391a9aed3a675bb1e
Sammy Robens-Paradise <srobensparadise@gmail.com>
2021-10-17 22:37:45 -0400

    f

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { Transition } from '@headlessui/react'
+import { Parallax } from 'react-scroll-parallax'
 
 import BlobElement from '../components/Blob/Blob'
 import { animate, cubicBezier } from '../global/helpers/animation'
@@ -19,49 +20,55 @@ const LandingPage: NextPage = (): JSX.Element => (
     <Head>
       <title>Sammy</title>
     </Head>
-    <Transition
-      className="grid grid-cols-2 gap-4"
-      show
-      appear
-      enter={`${animate(1000)} ${cubicBezier(0.97, 0.03, 0.36, 0.45)}`}
-      enterFrom="opacity-0"
-      enterTo="opaciy-100"
-    >
-      <div className="p-24 pt-48">
-        <h1
-          className="text-off-white font-work font-extrabold w-2/3 absolute z-20 tracking-tighter"
-          style={styleOffsetOverride}
+    <div className="relative">
+      <Parallax y={[-20, 20]}>
+        <Transition
+          className="grid grid-cols-2 gap-4 h-full relative"
+          show
+          appear
+          enter={`${animate(1000)} ${cubicBezier(0.97, 0.03, 0.36, 0.45)}`}
+          enterFrom="opacity-0"
+          enterTo="opaciy-100"
         >
-          {LANDING_MESSAGE}
-        </h1>
-        <h1 className="text-cerulaen font-work font-extrabold text-7xl w-2/3 absolute z-20 tracking-tighter">
-          {LANDING_MESSAGE}
-        </h1>
-      </div>
-      <div>
-        <div className="flex">
-          <BlobElement
-            color="#2B2B2B black"
-            className="absolute"
-            radius={250}
-            height={500}
-          />
-          <BlobElement
-            color="#3F46F3"
-            className="absolute blue"
-            radius={200}
-            height={500}
-          />
-          <BlobElement
-            color="#3FF3B2"
-            className="absolute green"
-            radius={150}
-            height={500}
-          />
-        </div>
-      </div>
-    </Transition>
-    <div>
+          <div className="p-24 pt-48">
+            <h1
+              className="text-off-white font-work font-extrabold w-2/3 absolute z-20 tracking-tighter"
+              style={styleOffsetOverride}
+            >
+              {LANDING_MESSAGE}
+            </h1>
+            <h1 className="text-cerulaen font-work font-extrabold text-7xl w-2/3 absolute z-20 tracking-tighter">
+              {LANDING_MESSAGE}
+            </h1>
+          </div>
+          <div>
+            <Parallax y={[-50, 50]}>
+              <div className="flex">
+                <BlobElement
+                  color="#2B2B2B black"
+                  className="absolute"
+                  radius={250}
+                  height={500}
+                />
+                <BlobElement
+                  color="#3F46F3"
+                  className="absolute blue"
+                  radius={200}
+                  height={500}
+                />
+                <BlobElement
+                  color="#3FF3B2"
+                  className="absolute green"
+                  radius={150}
+                  height={500}
+                />
+              </div>
+            </Parallax>
+          </div>
+        </Transition>
+      </Parallax>
+    </div>
+    <div className="relative">
       <Table>
         <p>1</p>
         <p>1</p>
