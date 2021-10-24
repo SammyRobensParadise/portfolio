651bdea6dac56cef49b2d658f8231e894f982cec
Sammy Robens-Paradise <srobensparadise@gmail.com>
2021-10-23 23:52:17 -0400

    added element

--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -1,31 +1,31 @@
-import React, { PropsWithChildren } from 'react'
+import React, { PropsWithChildren, forwardRef, ForwardedRef } from 'react'
 
-const Table = ({
-  children
-}: PropsWithChildren<Record<string, unknown>>): JSX.Element | null => {
-  if (children) {
-    const childrens = children as React.ReactElement[]
-    const thead = childrens.length
-      ? childrens.filter((child) => child?.type === 'thead')
-      : childrens
-    const trs = childrens.length
-      ? childrens.filter((child) => child?.type === 'tr')
-      : [childrens]
-
-    return (
-      <div className="container text-cerulaen dark:text-off-white-24">
-        <table className="table-auto w-full">
-          {thead}
-          <tbody className="border border-cerulaen p-4 border-collapse">
-            {trs.map((tr) => (
-              <>{tr}</>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    )
-  }
-  return null
+export type TableHeader = string
+export type TableHeaders = TableHeader[]
+export type TableRowItem = {
+  name: string
+  event?: () => void
+  type: 'text' | 'style'
 }
+export type TableRowItems = TableRowItem[]
+export type TableRows = TableRowItems[]
+
+export interface TableInterface {
+  classNames?: string
+  rows: TableRows
+}
+
+const Table = forwardRef(
+  (
+    props: PropsWithChildren<TableInterface>,
+    ref: ForwardedRef<HTMLTableElement>
+  ): JSX.Element => (
+    <div className="container text-cerulaen dark:text-off-white-24">
+      <table className="table-auto w-full" ref={ref}>
+        <tbody className="border border-cerulaen p-4 border-collapse" />
+      </table>
+    </div>
+  )
+)
 
 export default Table
