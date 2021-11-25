57dd538bc053c054a80165f33cb0b0fdc4b3c05d
Sammy Robens-Paradise <srobensparadise@gmail.com>
2021-11-25 11:20:05 -0500

    added table

--- components/Dialog/OverlayDialog.tsx
+++ components/Dialog/OverlayDialog.tsx
@@ -2,8 +2,18 @@ import React, { useState, useEffect, Fragment } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { useRouter } from 'next/router'
 
+import Table from '../Table/Table'
+import useTransition from '../../hooks/transition'
+
+export const Title: Record<string, string> = {
+  projects: 'Projects',
+  work: 'Work'
+}
+
 export default function OverlayDialog(): JSX.Element {
   const router = useRouter()
+  const { visibility, handlePageTransition } = useTransition({ timeout: 1000 })
+
   const [isOpen, setIsOpen] = useState<boolean>(false)
 
   function closeModal() {
@@ -18,11 +28,14 @@ export default function OverlayDialog(): JSX.Element {
       }
     }
   }, [router, router.query])
+
+  const type: string = router?.query.type as string
+
   return (
     <Transition appear show={isOpen} as={Fragment}>
       <Dialog
         as="div"
-        className="fixed inset-0 z-50"
+        className="fixed inset-0 z-50 mt-12"
         open={isOpen}
         onClose={closeModal}
       >
@@ -36,7 +49,7 @@ export default function OverlayDialog(): JSX.Element {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Dialog.Overlay className="transition transform fixed inset-0 bg-cerulaen opacity-0" />
+            <Dialog.Overlay className="transition transform fixed inset-0 bg-off-white dark:bg-shadow" />
           </Transition.Child>
 
           {/* This element is to trick the browser into centering the modal contents. */}
@@ -53,9 +66,73 @@ export default function OverlayDialog(): JSX.Element {
             leave="ease-in duration-200"
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
-            className="relative bg-off-white rounded max-w-sm mx-auto inline-block shadow"
+            className="relative bg-off-white dark:bg-shadow rounded mx-auto inline-block shadow"
           >
-            <div id="dialog">test</div>
+            <Dialog.Description className=" p-12">
+              <Table
+                id="work-table"
+                headers={['Work']}
+                rows={[
+                  {
+                    onClick: () => {
+                      closeModal()
+                      handlePageTransition('/beacon-biosignals')
+                    },
+                    elements: [
+                      {
+                        name: 'Beacon Biosignals',
+                        type: 'text'
+                      },
+                      {
+                        type: 'style',
+                        name: 'Revolutionizing end-to-end clinician workflows with interactive data visualization and machine learning.'
+                      },
+                      { name: "'21 - Present", type: 'text' }
+                    ]
+                  },
+                  {
+                    elements: [
+                      { name: 'Float Card', type: 'text' },
+                      {
+                        type: 'style',
+                        name: 'Bringing expense managment to the future.'
+                      },
+                      { name: "'20 - '21", type: 'text' }
+                    ]
+                  },
+                  {
+                    elements: [
+                      { name: 'PUMA', type: 'text' },
+                      {
+                        type: 'style',
+                        name: 'Leading better design for data scientists'
+                      },
+                      { name: "'20 - '20", type: 'text' }
+                    ]
+                  },
+                  {
+                    elements: [
+                      { name: 'Hootsuite', type: 'text' },
+                      {
+                        type: 'style',
+                        name: "Kickstarting Hootsuite's W3C AA/AAA accessibility implementation strategy."
+                      },
+                      { name: "'19 - '19", type: 'text' }
+                    ]
+                  },
+                  {
+                    elements: [
+                      { name: 'Unity, Finger Foods', type: 'text' },
+                      {
+                        type: 'style',
+                        name: 'AR, VR, XR, computer vision and your childhood toys re-imagined '
+                      },
+                      { name: "'18 - '19", type: 'text' }
+                    ]
+                  }
+                ]}
+              />
+            </Dialog.Description>
           </Transition.Child>
         </div>
       </Dialog>
