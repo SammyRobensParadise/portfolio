8484662ac5ae855db1945bf3ae5cd863668d7fb8
Sammy Robens-Paradise <srobensparadise@gmail.com>
2021-12-02 18:02:43 -0800

    updated 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -31,51 +31,6 @@ const NotFound: NextPage = (): JSX.Element => (
             Page Not Found
           </h1>
         </Transition>
-        <Transition
-          className="grid justify-items-center py-8 relative"
-          show
-          appear
-          enter={`${animate(1000, 0)} ${cubicBezier(0.97, 0.03, 0.36, 0.45)}`}
-          leave={`${animate(1000, 0)} ${cubicBezier(0.97, 0.03, 0.36, 0.45)}`}
-          enterFrom="opacity-0"
-          enterTo="opaciy-75"
-          leaveTo="opacity-0"
-          leaveFrom="opacity-75"
-        >
-          <h1 className="text-cerulaen dark:text-off-white font-work font-extrabold text-7xl w-2/3 absolute z-20 tracking-tighter text-center opacity-75">
-            Page Not Found
-          </h1>
-        </Transition>
-        <Transition
-          className="grid justify-items-center py-8 relative"
-          show
-          appear
-          enter={`${animate(1000, 0)} ${cubicBezier(0.97, 0.03, 0.36, 0.45)}`}
-          leave={`${animate(1000, 0)} ${cubicBezier(0.97, 0.03, 0.36, 0.45)}`}
-          enterFrom="opacity-0"
-          enterTo="opaciy-50"
-          leaveTo="opacity-0"
-          leaveFrom="opacity-50"
-        >
-          <h1 className="text-cerulaen dark:text-off-white font-work font-extrabold text-7xl w-2/3 absolute z-20 tracking-tighter text-center opacity-50">
-            Page Not Found
-          </h1>
-        </Transition>
-        <Transition
-          className="grid justify-items-center py-8 relative"
-          show
-          appear
-          enter={`${animate(1000, 0)} ${cubicBezier(0.97, 0.03, 0.36, 0.45)}`}
-          leave={`${animate(1000, 0)} ${cubicBezier(0.97, 0.03, 0.36, 0.45)}`}
-          enterFrom="opacity-0"
-          enterTo="opaciy-25"
-          leaveTo="opacity-0"
-          leaveFrom="opacity-25"
-        >
-          <h1 className="text-cerulaen dark:text-off-white font-work font-extrabold text-7xl w-2/3 absolute z-20 tracking-tighter text-center opacity-25">
-            Page Not Found
-          </h1>
-        </Transition>
       </div>
     </div>
   </>
