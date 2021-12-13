4cc8f73f379c0534e8a6730ac9fc2152ac97738c
Sammy Robens-Paradise <srobensparadise@gmail.com>
2021-12-12 21:58:24 -0500

    added elements

--- pages/api/info.ts
+++ pages/api/info.ts
@@ -3,11 +3,17 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
+  emoji: string
+  favourite_food: string
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ): void {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({
+    name: 'Sammy Robens-Paradise',
+    emoji: '🐧',
+    favourite_food: 'sushi'
+  })
 }
