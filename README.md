ca21a01505cb3e60a141a0ddd05c1b919c5e90c2
Sammy Robens-Paradise <srobensparadise@gmail.com>
2023-09-22 23:52:08 -0400

    started adding styles

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Portfolio
 
-My Portfolio built with: `React`, `Next.js`, `Express.js`, `TypeScript`, `Three.js` and `TailwindCSS`!
+My Portfolio built with: `React`, `Next.js`, `Express.js`, `TypeScript`, `Three.js`
 
-<img src="https://i.ibb.co/GtVgcdv/Sammy-Biking-Grey.png" alt="Sammy-Biking-Grey" border="0">
-
-### URL: [sammy.world](https://sammy.world)
+### URL: [`sammy.world`](https://sammy.world)
 
 ## Local Development
 
@@ -25,9 +23,3 @@ yarn dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-## Deployment
-
-The easiest way to deploy Next.js apps is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,79 +1,18 @@
-import '../styles/globals.css'
-import '../styles/tv.scss'
-import '../styles/glitch.scss'
-import '../styles/three.scss'
-
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import Script from 'next/script'
-import React, { ReactElement, useState } from 'react'
+import React, { ReactElement } from 'react'
 import { ParallaxProvider } from 'react-scroll-parallax'
-import { Curtains } from 'react-curtains'
-import { Provider } from 'jotai'
+import { Provider as State } from 'jotai'
+import { Grommet, ThemeType } from 'grommet'
+import '../styles/global.css'
 
-import OldSchool from '../providers/oldschool'
-import OldSchoolRenderer from '../theme/Oldschool/OldschoolStyle'
-import GlitchRenderer from '../theme/Glitch/GlitchStyle'
-import NavigationBar from '../components/Navigation/NavigationBar'
-import Footer from '../components/Footer/Footer'
-import Sidebar from '../components/Sidebar/Sidebar'
-import OverlayDialog from '../components/Dialog/OverlayDialog'
-import MobileWarning from '../components/MobileWarning/MobileWarning'
-import Overlay from '../components/Overlay/Overlay'
+import theme from '../styles/theme'
 
 function Wrapper({ Component, pageProps }: AppProps): ReactElement | null {
-  const oldSchool = OldSchool.useOldSchool()
-  const [darkTheme, setIsDarkTheme] = useState<boolean>(false)
-
-  if (oldSchool) {
-    const { state } = oldSchool
-    if (state?.react95Visible && state?.glitchVisible) {
-      return (
-        <div className="h-screen dark:bg-shadow bg-off-white">
-          <Head>
-            <title>Sammy Robens-Paradise 💾</title>
-          </Head>
-          <OldSchoolRenderer />
-        </div>
-      )
-    }
-    if (!state?.react95Visible && state?.glitchVisible) {
-      return (
-        <div className="h-screen bg-off-white dark:bg-shadow">
-          <Head>
-            <title>Sammy Robens-Paradise 🌩️</title>
-          </Head>
-          <GlitchRenderer />
-        </div>
-      )
-    }
-
-    return (
-      <Provider>
-        <OverlayDialog />
-        <MobileWarning />
-        <Overlay />
-        <div className="bg-off-white dark:bg-shadow">
-          <NavigationBar handleDarkTheme={setIsDarkTheme} />
-          <Sidebar />
-          <Curtains pixelRatio={Math.min(1.5, window.devicePixelRatio)}>
-            {/* }   <AnimatedCursor
-              innerSize={12}
-              outerSize={14}
-              color={darkTheme ? '63, 243, 178' : '43,43,43'}
-              outerAlpha={0.2}
-              innerScale={0.7}
-              outerScale={5}
-              trailingSpeed={6}
-    /> */}
-            <Component {...pageProps} />
-          </Curtains>
-          <Footer />
-        </div>
-      </Provider>
-    )
-  }
-  return null
+  return (
+    <State>
+      <Component {...pageProps} />
+    </State>
+  )
 }
 
 interface App extends AppProps {
@@ -82,32 +21,11 @@ interface App extends AppProps {
 
 function Portfolio({ Component, pageProps, router }: App): ReactElement {
   return (
-    <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${
-          process.env.GOOGLE_ANALYTICS_API_KEY as string
-        }`}
-      />
-      <Script strategy="lazyOnload" id="script">
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          
-            gtag('config', ${process.env.GOOGLE_ANALYTICS_API_KEY as string});
-        `}
-      </Script>
-      <OldSchool.Provider>
-        <ParallaxProvider>
-          <Wrapper
-            Component={Component}
-            pageProps={pageProps}
-            router={router}
-          />
-        </ParallaxProvider>
-      </OldSchool.Provider>
-    </>
+    <Grommet full theme={theme as unknown as ThemeType}>
+      <ParallaxProvider>
+        <Wrapper Component={Component} pageProps={pageProps} router={router} />
+      </ParallaxProvider>
+    </Grommet>
   )
 }
 
