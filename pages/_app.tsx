9c5f05d726274b96481376a21143232cec7ba498
Sammy Robens-Paradise <srobensparadise@gmail.com>
2023-09-28 22:59:09 -0400

    added animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { ParallaxProvider } from 'react-scroll-parallax'
 import { Provider as State } from 'jotai'
 import '../styles/globals.css'
 import AnimatedCursor from 'react-animated-cursor'
+import { AnimatePresence } from 'framer-motion'
 
 function Wrapper({ Component, pageProps }: AppProps): ReactElement | null {
   return (
@@ -20,22 +21,24 @@ interface App extends AppProps {
 function Portfolio({ Component, pageProps, router }: App): ReactElement {
   return (
     <ParallaxProvider>
-      <AnimatedCursor
-        innerSize={60}
-        outerSize={160}
-        color="255, 255, 255"
-        outerAlpha={1}
-        innerScale={0.2}
-        outerScale={2.5}
-        outerStyle={{
-          mixBlendMode: 'exclusion'
-        }}
-        innerStyle={{
-          mixBlendMode: 'difference'
-        }}
-        trailingSpeed={12}
-      />
-      <Wrapper Component={Component} pageProps={pageProps} router={router} />
+      <AnimatePresence mode="wait" initial={false}>
+        <AnimatedCursor
+          innerSize={60}
+          outerSize={160}
+          color="255, 255, 255"
+          outerAlpha={1}
+          innerScale={0.2}
+          outerScale={2.5}
+          outerStyle={{
+            mixBlendMode: 'exclusion'
+          }}
+          innerStyle={{
+            mixBlendMode: 'difference'
+          }}
+          trailingSpeed={12}
+        />
+        <Wrapper Component={Component} pageProps={pageProps} router={router} />
+      </AnimatePresence>
     </ParallaxProvider>
   )
 }
--- pages/work.tsx
+++ pages/work.tsx
@@ -5,52 +5,20 @@ import { uniqueId } from 'lodash'
 import clsx from 'clsx'
 import { motion } from 'framer-motion'
 
-import Button from '../components/button'
-
 const LandingPage: NextPage = (): JSX.Element => {
   const [idx, setIdx] = useState(1)
   const [idx2, setIdx2] = useState(0)
-  const [idx3, setIdx3] = useState(0)
-
   const timeout = 200
 
-  const introText = [
-    'I',
-    'AM',
-    'SAMMY',
-    'ROBENS-PARADISE',
-    'AND',
-    'I',
-    'BUILD',
-    'BEAUTIFUL',
-    'PRODUCTS'
-  ]
+  const introText = ['THIS', 'IS', 'MY', 'WORK']
   const introText2 = ['THESE', 'ARE', 'YOUR', 'CHOICES']
-  const options: Array<{
-    label: string
-    variant: 'primary' | 'secondary'
-    anchor: boolean
-    href: string
-  }> = [
-    { label: 'SEE WORK', anchor: true, variant: 'secondary', href: '/work' },
-    {
-      label: 'VIEW RESUME',
-      anchor: true,
-      variant: 'secondary',
-      href: '/SammyRPResume.pdf'
-    },
-    { label: 'ABOUT ME', anchor: true, variant: 'secondary', href: '/about' }
-  ]
 
   useEffect(() => {
     let secInterval: NodeJS.Timeout
     let interval: NodeJS.Timeout
     let thirdInterval: NodeJS.Timeout
     const condition =
-      idx <= introText.length + 1 &&
-      idx2 <= introText2.length + 1 &&
-      idx3 <= options.length + 1
-
+      idx <= introText.length + 1 && idx2 <= introText2.length + 1
     if (condition) {
       interval = setInterval(() => {
         setIdx((s) => s + 1)
@@ -60,12 +28,6 @@ const LandingPage: NextPage = (): JSX.Element => {
         secInterval = setInterval(() => {
           if (idx2 >= introText2.length) {
             clearInterval(secInterval)
-            thirdInterval = setInterval(() => {
-              setIdx3((s) => s + 1)
-              if (idx3 >= options.length) {
-                clearInterval(thirdInterval)
-              }
-            }, timeout)
           } else {
             setIdx2((s) => s + 1)
           }
@@ -77,21 +39,21 @@ const LandingPage: NextPage = (): JSX.Element => {
       clearInterval(secInterval)
       clearInterval(thirdInterval)
     }
-  }, [idx, idx2, idx3, introText.length, introText2.length, options.length])
+  }, [idx, idx2, introText.length, introText2.length])
 
   return (
     <>
       <Head>
-        <title>Sammy Robens-Paradise</title>
+        <title>Work</title>
       </Head>
-      <div className=" bg-ocrean-green min-h-screen">
+      <div className=" bg-prussian-blue min-h-screen">
         <div className="text-center p-16  flex flex-col justify-center gap-16 items-center">
-          <h1 className="text-6xl font-bold text-prussian-blue tracking-tight  text-center leading-[78px] max-w-7xl selection:text-ruby selection:bg-prussian-blue">
+          <h1 className="text-6xl font-bold text-canary tracking-tight text-center leading-[78px] max-w-7xl selection:text-ruby selection:bg-ocean-green">
             {introText.slice(0, idx).map((word, index) => (
               <span
                 key={`${word}-${uniqueId()}`}
                 data-label={word.toLowerCase()}
-                className={clsx(index === idx - 1 && 'text-canary')}
+                className={clsx(index === idx - 1 && 'text-ocrean-green')}
               >
                 {word.split('').map((letter) => (
                   <motion.span
@@ -114,9 +76,7 @@ const LandingPage: NextPage = (): JSX.Element => {
                 <span
                   key={`${uniqueId()}`}
                   data-label={word.toLowerCase()}
-                  className={clsx(
-                    index === idx2 - 1 && idx3 <= 0 && 'text-canary'
-                  )}
+                  className={clsx(index === idx2 - 1 && 'text-canary')}
                 >
                   {word.split('').map((letter) => (
                     <motion.span
@@ -133,24 +93,6 @@ const LandingPage: NextPage = (): JSX.Element => {
                 </span>
               ))}
             </h2>
-            <div className="flex flex-col space-y-24 py-16 lg:space-x-24 lg:py-16 lg:block ">
-              {options.slice(0, idx3).map(({ label, href }, index) => (
-                <motion.span key={`${uniqueId()}`}>
-                  <Button
-                    anchor
-                    href={href}
-                    variant={
-                      clsx({
-                        secondary: index === idx3 - 1,
-                        primary: index !== idx3 - 1
-                      }) as 'primary' | 'secondary'
-                    }
-                  >
-                    {label}
-                  </Button>
-                </motion.span>
-              ))}
-            </div>
           </div>
         </div>
       </div>
