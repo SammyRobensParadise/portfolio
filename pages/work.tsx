690efe1c62a44b2f0faaf48f812d1189fcd8f5ba
Sammy Robens-Paradise <srobensparadise@gmail.com>
2023-09-30 20:56:18 -0400

    saved

--- pages/work.tsx
+++ pages/work.tsx
@@ -1,37 +1,43 @@
-import React, { forwardRef, useEffect, useState } from 'react'
+import React, { forwardRef, useEffect, useState, useRef } from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { uniqueId } from 'lodash'
 import clsx from 'clsx'
 import { motion } from 'framer-motion'
+import * as THREE from 'three'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { Image, ScrollControls, Scroll, useScroll } from '@react-three/drei'
+import { proxy, useSnapshot } from 'valtio'
+
+const { damp } = THREE.MathUtils
+const material = new THREE.LineBasicMaterial({ color: 'white' })
+const geometry = new THREE.BufferGeometry().setFromPoints([
+  new THREE.Vector3(0, -0.5, 0),
+  new THREE.Vector3(0, 0.5, 0)
+])
+const state = proxy({
+  clicked: null,
+  urls: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 5, 7, 8, 2, 4, 9, 6].map(
+    (u) => `/${u}.jpg`
+  )
+})
 
 const LandingPage: NextPage = forwardRef((): JSX.Element => {
   const [idx, setIdx] = useState(1)
-  const [idx2, setIdx2] = useState(0)
   const timeout = 200
 
   const introText = ['THIS', 'IS', 'MY', 'WORK']
-  const introText2 = ['THESE', 'ARE', 'YOUR', 'CHOICES']
-
   useEffect(() => {
     let secInterval: NodeJS.Timeout
     let interval: NodeJS.Timeout
     let thirdInterval: NodeJS.Timeout
-    const condition =
-      idx <= introText.length + 1 && idx2 <= introText2.length + 1
+    const condition = idx <= introText.length + 1
     if (condition) {
       interval = setInterval(() => {
         setIdx((s) => s + 1)
       }, timeout)
       if (idx >= introText.length + 1) {
         clearInterval(interval)
-        secInterval = setInterval(() => {
-          if (idx2 >= introText2.length) {
-            clearInterval(secInterval)
-          } else {
-            setIdx2((s) => s + 1)
-          }
-        }, timeout)
       }
     }
     return () => {
@@ -39,7 +45,7 @@ const LandingPage: NextPage = forwardRef((): JSX.Element => {
       clearInterval(secInterval)
       clearInterval(thirdInterval)
     }
-  }, [idx, idx2, introText.length, introText2.length])
+  }, [idx, introText.length])
 
   return (
     <>
