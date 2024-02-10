48ac66ae3fa57b369fe59fc95b97b59051fff36d
Sammy Robens-Paradise <srobensparadise@gmail.com>
2024-02-10 14:41:19 -0800

    continued development

--- pages/work.tsx
+++ pages/work.tsx
@@ -5,6 +5,7 @@ import { uniqueId } from 'lodash'
 import clsx from 'clsx'
 import { motion } from 'framer-motion'
 import { useInterval } from 'usehooks-ts'
+import Link from 'next/link'
 
 import Layout from '../components/Layout'
 
@@ -12,18 +13,74 @@ const timeout = 200
 
 const txt1 = ['THIS', 'IS', 'MY', 'WORK']
 
+const workOptions: Array<{
+  label: string
+  subtext: string
+  anchor: boolean
+  href: string
+}> = [
+  {
+    label: 'BEACON BIOSIGNALS',
+    subtext:
+      "Designing the world's AI neurobiomarker platform accelerating clinical trials and powering new treatments for patients with neurological and psychiatric diseases",
+    anchor: true,
+    href: '/beacon-biosignals'
+  },
+  {
+    label: 'FLOAT',
+    subtext:
+      'Orchestrating the dream-to-design-to-build pipeline for Float Inc on a mission to redesign business spending',
+    anchor: true,
+    href: '/float'
+  },
+  {
+    label: 'PHARMABOX',
+    subtext:
+      'A capstone project to deliver accessible prescription pickup for Canadians',
+    anchor: true,
+    href: '/pharmabox'
+  },
+  {
+    label: 'HOOTSUITE',
+    subtext:
+      'Developing and deploy code to 18 million users on a daily basis, driving product growth and an unparalleled tool belt for social media gurus.',
+    anchor: true,
+    href: '/hootsuite'
+  },
+  {
+    label: 'FINGER FOOD STUDIOS',
+    subtext:
+      'I developed a system to quantify UX for world class brands like LEGO, and led QA infrastructure development on bleeding edge mixed reality projects',
+    anchor: true,
+    href: '/finger-food-studios'
+  }
+]
+
 const Work = forwardRef((): JSX.Element => {
   const [txt1Index, setTxt1Index] = useState(1)
+  const [optionsIndex, setOptionsIndex] = useState(0)
   const [playText1, setPlayText1] = useState(true)
+  const [playOptions, setPlayOptions] = useState(false)
 
   useInterval(
     () => {
       setTxt1Index((i) => i + 1)
       if (txt1Index >= txt1.length) {
         setPlayText1(false)
+        setPlayOptions(true)
       }
     },
     playText1 ? timeout : null
+  )
+
+  useInterval(
+    () => {
+      setOptionsIndex((j) => j + 1)
+      if (optionsIndex >= workOptions.length) {
+        setPlayOptions(false)
+      }
+    },
+    playOptions ? timeout : null
   )
 
   return (
@@ -32,7 +89,7 @@ const Work = forwardRef((): JSX.Element => {
         <title>Work</title>
       </Head>
       <div className=" bg-prussian-blue min-h-screen overflow-y-hidden">
-        <div className="text-center p-16  flex flex-col justify-center gap-16 items-center">
+        <div className="text-center p-16 z-10 flex flex-col justify-center gap-16 items-center">
           <h1 className="text-6xl font-bold text-canary tracking-tight text-center leading-[78px] max-w-7xl selection:text-ruby selection:bg-ocean-green fixed">
             {txt1.slice(0, txt1Index).map((word, index) => (
               <span
@@ -56,6 +113,43 @@ const Work = forwardRef((): JSX.Element => {
             ))}
           </h1>
         </div>
+        {optionsIndex > 0 && (
+          <div className="text-center z-20">
+            <div className="text-6xl py-8 relative font-bold text-canary tracking-tight text-center leading-[78px]  selection:text-ruby selection:bg-ocean-green bg-prussian-blue ">
+              {workOptions
+                .slice(0, optionsIndex)
+                .map(({ label, href }, index) => (
+                  <motion.div
+                    whileHover={{
+                      borderBottom: '4px solid',
+                      borderTop: '4px solid'
+                    }}
+                    transition={{ type: 'spring', stiffness: 400, damping: 10 }}
+                    key={`${label}-${uniqueId()}`}
+                    data-label={label.toLowerCase()}
+                    className={` py-1 border-prussian-blue border-4 ${clsx(
+                      index === optionsIndex - 1 && 'text-ocrean-green'
+                    )}`}
+                  >
+                    <Link href={href}>
+                      {label.split('').map((letter) => (
+                        <motion.span
+                          key={`${letter}-${uniqueId()}`}
+                          whileHover={{
+                            fontSize: '64px',
+                            lineHeight: '48px',
+                            letterSpacing: '-2.5px'
+                          }}
+                        >
+                          {letter}
+                        </motion.span>
+                      ))}{' '}
+                    </Link>
+                  </motion.div>
+                ))}
+            </div>
+          </div>
+        )}
       </div>
     </>
   )
