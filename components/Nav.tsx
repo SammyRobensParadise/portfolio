7454b3f3924fd3ec2c1c44bf0e391daa9113d60a
Sammy Robens-Paradise <srobensparadise@gmail.com>
2024-07-24 20:42:35 -0700

    f

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,14 +1,26 @@
+import { useState } from 'react'
+
+import Sidebar from './Sidebar'
+
 export default function Navbar({
   color = 'border-prussian-blue'
 }: {
   color?: string
 }): JSX.Element {
+  const [sidebarOpen, setSidebarOpen] = useState<boolean>(false)
+
+  function closeSidebar() {
+    setSidebarOpen(false)
+  }
+
   return (
     <div>
+      {sidebarOpen && <Sidebar onClose={() => closeSidebar()} />}
       <button
         aria-label="home"
         type="button"
         className={`h-12 w-12 transition-all ease-in-out bg-off-white border-[16px] hover:border-8 n ${color}`}
+        onClick={() => setSidebarOpen(true)}
       />
     </div>
   )
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,14 +1,32 @@
-export default function Sidebar(): JSX.Element {
+import { X } from 'lucide-react'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+
+export default function Sidebar({
+  onClose
+}: {
+  onClose: () => void
+}): JSX.Element {
   return (
     <div className="fixed flex h-full flex-col bg-prussian-blue w-[50%]">
-      <div className="p-4 mb-2">
-        <h5 className="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-          Sidebar
-        </h5>
+      <div className="p-8 mb-2 flex justify-between items-center">
+        <motion.button
+          whileHover={{
+            scaleX: 1.1
+          }}
+          className="block font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900 text-off-white"
+        >
+          <Link href="/">RETURN HOME</Link>
+        </motion.button>
+        <button type="button" onClick={onClose} className="text-off-white">
+          <X size={32} />
+        </button>
       </div>
-      <nav className="flex min-w-[240px] flex-col gap-1 p-2 font-sans text-base font-normal text-blue-gray-700">
-        content
-      </nav>
+      <div className="p-8">
+        <nav className="flex min-w-[240px] flex-col gap-1 p-2 font-sans text-base font-normal text-blue-gray-700">
+          content
+        </nav>
+      </div>
     </div>
   )
 }
